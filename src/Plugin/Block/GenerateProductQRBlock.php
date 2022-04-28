5217a58954987b33f2a5b71d606d3d61814bbc4b
spaurnima <104417004+spaurnima@users.noreply.github.com>
2022-04-28 10:39:43 +0530

    Updated GenerateProductQRBlock.php file

--- src/Plugin/Block/GenerateProductQRBlock.php
+++ src/Plugin/Block/GenerateProductQRBlock.php
@@ -1,18 +1,5 @@
 <?php
 
-/**
- * GenerateProductQRBlock File Doc Comment.
- *
- * PHP version  7.4.3
- *
- * @description File used to generate QR Code
- * @category Block_Page_For_QR_Code_Generation
- * @package GenerateProductQRBlock
- * @author Author <paurnimas@cybage.com>
- * @license https://opensource.org/licenses/MIT MIT License
- * @link Drupal\jugaad_patches\Plugin\Block
- */
-
 namespace Drupal\jugaad_patches\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
@@ -28,12 +15,14 @@ use Endroid\QrCode\Writer\PngWriter;
 use Endroid\QrCode\Color\Color;
 use Endroid\QrCode\Label\Label;
 use Psr\Log\LoggerInterface;
+use Drupal\node\NodeInterface;
 use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
 
 define('UPLOAD_DIR', 'sites/default/files/assets/');
 
 /**
- * Template Class Doc Comment
+ * Template Class Doc Comment.
+ *
  * Provides a block with a QR code generation.
  *
  * @category GenerateProductQRBlock
@@ -48,118 +37,119 @@ define('UPLOAD_DIR', 'sites/default/files/assets/');
  */
 class GenerateProductQRBlock extends BlockBase implements
     ContainerFactoryPluginInterface {
-/**
- * RouteMatch used to get parameter Node.
- *
- * @var \Drupal\Core\Routing\RouteMatchInterface
- * @package GenerateProductQRBlock
- * @category QR Code generation Block php
- * @license https://opensource.org/licenses/MIT MIT License
- * @link Drupal\jugaad_patches\Plugin\Block
- */
-protected $routeMatch;
+  /**
+   * RouteMatch used to get parameter Node.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   * @package GenerateProductQRBlock
+   * @category QR Code generation Block php
+   * @license https://opensource.org/licenses/MIT MIT License
+   * @link Drupal\jugaad_patches\Plugin\Block
+   */
+  protected $routeMatch;
 
-/**
- * Constructs a new Block Object.
- *
- * @param array $configuration
- *   Information about the plugin instance.
- * @param string $plugin_id
- *   The plugin_id for the plugin instance.
- * @param string $plugin_definition
- *   The plugin implementation definition.
- * @param string $route_match
- *   Route match variable.
- * @param string $logger
- *   Logger variable.
- */
-public function __construct(array $configuration,
-  $plugin_id,
-  array $plugin_definition,
-  RouteMatchInterface $route_match,
-  LoggerInterface $logger
-    ) {
-  parent::__construct($configuration, $plugin_id, $plugin_definition);
-  $this->routeMatch = $route_match;
+  /**
+   * Constructs a new Block Object.
+   *
+   * @param array $configuration
+   *   Information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param array $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   $route_match Route match variable.
+   * @param Psr\Log\LoggerInterface $logger
+   *   Logger variable.
+   */
+  public function __construct(array $configuration,
+    $plugin_id,
+    array $plugin_definition,
+    RouteMatchInterface $route_match,
+    LoggerInterface $logger
+      ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->routeMatch = $route_match;
 
-}
+  }
 
-/**
- * {@inheritdoc}
- *
- * @param string $container
- *   ID for plugin.
- * @param array $configuration
- *   Array of configuration.
- * @param string $plugin_id
- *   Container variable.
- * @param string $plugin_definition
- *   Defination of plugin.
- *
- * @return static
- *   static values
- */
-public static function create(ContainerInterface $container,
-  array $configuration,
-  $plugin_id,
-  $plugin_definition) {
-  return new static(
+  /**
+   * {@inheritdoc}
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   $container ID for plugin.
+   * @param array $configuration
+   *   Array of configuration.
+   * @param string $plugin_id
+   *   Container variable.
+   * @param string $plugin_definition
+   *   Defination of plugin.
+   *
+   * @return static
+   *   static values
+   */
+  public static function create(ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition) {
+    return new static(
             $configuration,
             $plugin_id,
             $plugin_definition,
             $container->get('current_route_match'),
             $container->get('logger.factory')->get('jugaad_patches'),
         );
-}
+  }
 
-/**
- * {@inheritdoc}
- *
- * @return string
- *   QR Code
- */
-public function build() {
-  $node = $this->routeMatch->getParameter('node');
-  // Create a basic QR code.
-  if ($node instanceof \Drupal\node\NodeInterface) {
-    // Condition for Products content type.
-    if ($node->bundle() == 'products') {
-      $nid = $node->id();
-      $qrUrl = $node->get('field_app_purchase_link')->getValue()[0]['uri'];
-      if (!empty($qrUrl)) {
-        try {
-          $qrCode = new QrCode($qrUrl);
-          $qrCode->setErrorCorrectionLevel(
-                            new ErrorCorrectionLevelLow()
-                        )
-            ->setSize(300)
-            ->setMargin(10)
-            ->setRoundBlockSizeMode(new RoundBlockSizeModeMargin())
-            ->setEncoding(new Encoding('UTF-8'))
-            ->setForegroundColor(new Color(0, 0, 0))
-            ->setBackgroundColor(new Color(255, 255, 255));
-          $writer = new PngWriter();
-          // Create generic label.
-          $label = Label::create('Scan here to buy')
-             ->setTextColor(new Color(255, 0, 0));
-          $result = $writer->write($qrCode, NULL, $label);
-          header("Content-Type: " . $result->getMimeType());
-          $dataUri = $result->getDataUri();
-          $img = str_replace('data:image/png;base64,', '', $dataUri);
-          $img = str_replace(' ', '+', $img);
-          $data = base64_decode($img);
-          $path = "sites/default/files/assets/";
-          if (!is_dir($path)) {
-            mkdir($path, 0777, TRUE);
+  /**
+   * {@inheritdoc}
+   *
+   * @return string
+   *   QR Code
+   */
+  public function build() {
+    $node = $this->routeMatch->getParameter('node');
+    // Create a basic QR code.
+    if ($node instanceof NodeInterface) {
+      // Condition for Products content type.
+      if ($node->bundle() == 'products') {
+        $nid = $node->id();
+        $qrUrl = $node->get('field_app_purchase_link')->getValue()[0]['uri'];
+        if (!empty($qrUrl)) {
+          try {
+            $qrCode = new QrCode($qrUrl);
+            $qrCode->setErrorCorrectionLevel(
+                              new ErrorCorrectionLevelLow()
+                          )
+              ->setSize(300)
+              ->setMargin(10)
+              ->setRoundBlockSizeMode(new RoundBlockSizeModeMargin())
+              ->setEncoding(new Encoding('UTF-8'))
+              ->setForegroundColor(new Color(0, 0, 0))
+              ->setBackgroundColor(new Color(255, 255, 255));
+            $writer = new PngWriter();
+            // Create generic label.
+            $label = Label::create('Scan here to buy')
+              ->setTextColor(new Color(255, 0, 0));
+            $result = $writer->write($qrCode, NULL, $label);
+            header("Content-Type: " . $result->getMimeType());
+            $dataUri = $result->getDataUri();
+            $img = str_replace('data:image/png;base64,', '', $dataUri);
+            $img = str_replace(' ', '+', $img);
+            $data = base64_decode($img);
+            $path = "sites/default/files/assets/";
+            if (!is_dir($path)) {
+              mkdir($path, 0777, TRUE);
+            }
+            $file = UPLOAD_DIR . $nid . '.png';
+            $success = file_put_contents($file, $data);
           }
-          $file = UPLOAD_DIR . $nid . '.png';
-          $success = file_put_contents($file, $data);
           catch (Exception $e) {
             // Log the exception to watchdog.
             $err = Error::decodeException($e);
             $this->logger->error(
-                            '%type: @message in %function 
-                        (line %line of %file).', $ex_vars
+                              '%type: @message in %function 
+                          (line %line of %file).', $ex_vars
             );
           }
         }
@@ -174,10 +164,10 @@ public function build() {
   /**
    * {@inheritdoc}
    *
-   * @param string $account
+   * @param \Drupal\Core\Session\AccountInterface $account
    *   This variable holds account value.
    *
-   * @return AccessResult
+   * @return \Drupal\Core\Access\AccessResult
    *   If User can access content
    */
   protected function blockAccess(AccountInterface $account) {
